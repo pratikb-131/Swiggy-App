901774ad4b0f9c29257ec22aea55043c72667f28
Pratik Bachhav <patrik.13104@gmail.com>
2026-01-19 00:20:39 +0530

    class based componets introduced

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,13 @@
+import { useEffect } from "react";
+
 const Header = ( ) => {
+    
+    useEffect(() => {
+        setInterval(() => {
+        console.log("Hello");
+        }, 1000);
+    },[])
+
     return(
     <div >
         This is header
